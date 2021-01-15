3151e264ad33ec377784975b5d76795a4fe9a3d0
Nardi Lam <mail@nardilam.nl>
2021-01-15 18:40:27 +0100

    Abstracted out ZMQ stuff from agents

--- ajay.py
+++ ajay.py
@@ -6,34 +6,50 @@ from asyncio import run, gather, sleep
 from zmq import PULL, PUSH
 import zmq.asyncio as zmq
 
-async def sender():
+context = zmq.Context()
+
+receiver_port = 5555
+sender_port = 6666
+
+async def run_agent(name, port, func):
+    print(f"-{name}-  Creating socket")
+    inbox = context.socket(PULL)
+    inbox.bind(f"tcp://*:{port}")
+
+    print(f"-{name}-  Starting agent")
+    await func(inbox)
+
+async def connect_agent(port):
+    outbox = context.socket(PUSH)
+    outbox.connect(f"tcp://localhost:{port}")
+    return outbox
+
+async def sender(inbox):
     print("-S-  Started")
     
     await sleep(1)
-    print("-S-  Connecting to server…")
-    context = zmq.Context()
-    socket = context.socket(PUSH)
-    socket.connect("tcp://localhost:5555")
+
+    print("-S-  Connecting to other agent...")
+    outbox = await connect_agent(receiver_port)
 
     print("-S-  Sending message")
-    await socket.send(b"Hello")
+    await outbox.send(b"Hello")
 
     print("-S-  Done")
 
-async def receiver():
+async def receiver(inbox):
     print("-R-  Started")
 
-    context = zmq.Context()
-    socket = context.socket(PULL)
-    socket.bind("tcp://*:5555")
-
     print("-R-  Waiting for message")
-    message = await socket.recv()
+    message = await inbox.recv()
     print("-R-  Received message: %s" % message)
 
     print("-R-  Done")
 
 async def main():
-    await gather(receiver(), sender())
+    await gather(
+        run_agent("R", receiver_port, receiver),
+        run_agent("S", sender_port, sender)
+    )
 
 run(main())
