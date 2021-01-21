d4df7e5a39a82c5c30916be39e6200059e9b166f
Nardi Lam <mail@nardilam.nl>
2021-01-21 22:31:06 +0100

    Removed pairwise from utils (not necessary)

--- ajay/utils.py
+++ ajay/utils.py
@@ -4,9 +4,3 @@ from asyncstdlib.itertools import tee, zip_longest
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
-
-async def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    await anext(b, None)
-    return zip(a, b)
